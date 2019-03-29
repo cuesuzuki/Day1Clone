e2d2ce9175a749df094cd7e1833c6bf9c8c47d87
yutaka suzuki <cue.suzuki@gmail.com>
2019-03-29 22:43:35 +0900

    Commit title here

---
+++ DayOneTest/JournalDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  JournalDetailViewController.swift
+//  DayOneTest
+//
+//  Created by yutaka suzuki on 2019/03/29.
+//  Copyright © 2019 yutaka suzuki. All rights reserved.
+//
+
+import UIKit
+
+class JournalDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
